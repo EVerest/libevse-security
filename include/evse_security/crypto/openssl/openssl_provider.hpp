f2c7d71328528532c78ce7446514c5a338ab10dc
AssemblyJohn <ioan.bogdann@gmail.com>
2024-07-23 16:05:04 +0300

    Prepared library for custom provider integration
    Signed-off-by: AssemblyJohn <ioan.bogdann@gmail.com>

--- include/evse_security/crypto/openssl/openssl_provider.hpp
+++ include/evse_security/crypto/openssl/openssl_provider.hpp
@@ -21,13 +21,13 @@ namespace evse_security {
 /// @param private_key_pem string containing the PEM encoded key
 /// @return true when "-----BEGIN TSS2 PRIVATE KEY-----" found
 /// @note works irrespective of OpenSSL version
-bool is_tpm_key_string(const std::string& private_key_pem);
+bool is_custom_key_string(const std::string& private_key_pem);
 
 /// @brief determine if the PEM file contains a TSS2 private key
 /// @param private_key_file_pem filename of the PEM file
 /// @return true when file starts "-----BEGIN TSS2 PRIVATE KEY-----"
 /// @note works irrespective of OpenSSL version
-bool is_tpm_key_file(const fs::path& private_key_file_pem);
+bool is_custom_key_file(const fs::path& private_key_file_pem);
 
 /// @brief Manage the loading and configuring of OpenSSL providers
 ///
@@ -55,25 +55,25 @@ public:
     /// @brief supported propquery strings
     enum class mode_t {
         default_provider,
-        tpm2_provider,
+        custom_provider,
     };
 
 private:
     typedef std::uint8_t flags_underlying_t;
     enum class flags_t : flags_underlying_t {
         initialised,
-        tpm2_available,
-        global_tpm2,
-        tls_tpm2,
+        custom_provider_available,
+        global_custom_provider,
+        tls_custom_provider,
     };
 
     static std::mutex s_mux;
     static flags_underlying_t s_flags;
 
     static struct ossl_provider_st* s_global_prov_default_p;
-    static struct ossl_provider_st* s_global_prov_tpm_p;
+    static struct ossl_provider_st* s_global_prov_custom_p;
     static struct ossl_provider_st* s_tls_prov_default_p;
-    static struct ossl_provider_st* s_tls_prov_tpm_p;
+    static struct ossl_provider_st* s_tls_prov_custom_p;
     static struct ossl_lib_ctx_st* s_tls_libctx_p;
 
     static inline void reset(flags_t f) {
@@ -97,22 +97,22 @@ private:
     /// @param val - whether to set or reset the flag
     /// @return true when the flag was changed
     static inline bool update(flags_t f, bool val) {
-        bool bResult = val != is_set(f);
+        bool result = (val != is_set(f));
         if (val) {
             set(f);
         } else {
             reset(f);
         }
-        return bResult;
+        return result;
     }
 
     bool load(struct ossl_provider_st*& default_p, struct ossl_provider_st*& tpm2_p, struct ossl_lib_ctx_st* libctx_p,
               mode_t mode);
     inline bool load_global(mode_t mode) {
-        return load(s_global_prov_default_p, s_global_prov_tpm_p, nullptr, mode);
+        return load(s_global_prov_default_p, s_global_prov_custom_p, nullptr, mode);
     }
     inline bool load_tls(mode_t mode) {
-        return load(s_tls_prov_default_p, s_tls_prov_tpm_p, s_tls_libctx_p, mode);
+        return load(s_tls_prov_default_p, s_tls_prov_custom_p, s_tls_libctx_p, mode);
     }
 
     bool set_propstr(struct ossl_lib_ctx_st* libctx, mode_t mode);
@@ -133,10 +133,10 @@ public:
     const char* propquery(mode_t mode) const;
 
     inline mode_t propquery_global() const {
-        return (is_set(flags_t::global_tpm2)) ? mode_t::tpm2_provider : mode_t::default_provider;
+        return (is_set(flags_t::global_custom_provider)) ? mode_t::custom_provider : mode_t::default_provider;
     }
     inline mode_t propquery_tls() const {
-        return (is_set(flags_t::tls_tpm2)) ? mode_t::tpm2_provider : mode_t::default_provider;
+        return (is_set(flags_t::tls_custom_provider)) ? mode_t::custom_provider : mode_t::default_provider;
     }
 
     inline const char* propquery_global_str() const {
@@ -151,8 +151,16 @@ public:
         return s_tls_libctx_p;
     }
 
-    static inline bool supports_tpm() {
-        return is_set(flags_t::tpm2_available);
+    static inline bool supports_provider_tpm() {
+#if USING_CUSTOM_PROVIDER
+        return is_set(flags_t::custom_provider_available) && (0 == std::strcmp(CUSTOM_PROVIDER_NAME, "tmp2"));
+#else
+        return false;
+#endif
+    }
+
+    static inline bool supports_provider_custom() {
+        return is_set(flags_t::custom_provider_available);
     }
 
     static void cleanup();
